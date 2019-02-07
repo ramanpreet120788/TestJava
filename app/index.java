87b22831ba42fe46a31757a8186ae8da166f7ef9
Raman Preet <raman.preet@nagarro.com>
2019-02-07 11:22:38 +0530

    Readme 2 added

--- app/index.java
+++ app/index.java
@@ -1,12 +1,26 @@
+import java.util.Scanner;
 
 public class index {
 
 public static void main( String[] args )
     {
-	
-        System.out.println("Hello World");	
-  
+ int a=1;
+        int b=2;
+        System.out.println("Enter how many numers you want to print"); 
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        
+        // Printing the read line 
+        System.out.println(number);  
+        System.out.println(a);	
+        System.out.println(b);	
+        for(int i=0;i<number;++i)
+        {
+        int c=a+b;
+        System.out.println(c);
+        a=b;
+        b=c;
     }
-
+}
 
 }
